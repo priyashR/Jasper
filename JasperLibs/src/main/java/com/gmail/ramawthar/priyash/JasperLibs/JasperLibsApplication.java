79ca9d8360ac613331ce6c7a07b1e180a4d6010c
priyashR <priyash.ramawthar@gmail.com>
2024-11-25 16:19:24 +0200

    jasperLibs project

---
+++ JasperLibs/src/main/java/com/gmail/ramawthar/priyash/JasperLibs/JasperLibsApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.ramawthar.priyash.JasperLibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperLibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperLibsApplication.class, args);
+	}
+
+}
---
+++ JasperLibs/src/test/java/com/gmail/ramawthar/priyash/JasperLibs/JasperLibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.ramawthar.priyash.JasperLibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperLibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
